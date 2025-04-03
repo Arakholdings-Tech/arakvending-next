8ed4eca194554262e06cb31f80d260b7f1d01007
Given Ncube <givenyslim12@gmail.com>
2025-04-03 13:02:19 +0200

    wip

--- config/initializers/vending_routes.rb
+++ config/initializers/vending_routes.rb
@@ -1,11 +1,11 @@
 Rails.application.config.to_prepare do
   Vending::Transport.routes.draw do
-    command 'POLL', to: 'vending/ack#handle'
-    command 'SELECT_SELECTION', to: 'vending/selection#selected'
-    command 'SET_SELECTION_INVENTORY', to: 'vending/selection#inventory'
-    command 'MACHINE_STATUS', to: 'vending/machine#status'
-    command 'SET_SELECTION_PRICE', to: 'vending/selection#price'
-    command 'DISPENSING_STATUS', to: 'vending/selection#dispensing'
-    command 'ACK', to: 'vending/ack#handle'
+    command "POLL", to: "vending/ack#handle"
+    command "SELECT_SELECTION", to: "vending/selection#selected"
+    command "SET_SELECTION_INVENTORY", to: "vending/selection#inventory"
+    command "MACHINE_STATUS", to: "vending/machine#status"
+    command "SET_SELECTION_PRICE", to: "vending/selection#price"
+    command "DISPENSING_STATUS", to: "vending/selection#dispensing"
+    #    command 'ACK', to: 'vending/ack#handle'
   end
 end
